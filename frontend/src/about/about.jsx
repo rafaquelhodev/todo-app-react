176b2ef0435381e112957364b92f0d487400cf4b
rafael.quelho <rafael.quelho@norus.com.br>
2020-10-12 12:11:14 -0300

    page header create

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from "react"
+import PageHeader from "../template/pageHeader"
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name="About" small="us"></PageHeader>
+        <h2>Our history</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, rem, dolore impedit incidunt quos vitae nesciunt, consectetur officia tempore veniam excepturi aut fugiat ipsum voluptatibus velit doloremque? In, autem eius.</p>
+
+        <h2>Mission</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus quas nemo mollitia eveniet commodi blanditiis corporis. Voluptatum officiis esse, dicta expedita sequi consectetur natus quibusdam voluptatibus, ab totam suscipit amet?</p>
+
     </div>
 )
