2f34778d6ef0d2e115049d13fcf27ed70d88675e
rafael.quelho <rafael.quelho@norus.com.br>
2020-10-12 12:02:38 -0300

    menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,10 +4,10 @@ import "modules/font-awesome/css/font-awesome.min.css"
 import React from "react"
 import Todo from "../todo/todo"
 import About from "../about/about"
+import Menu from "../template/menu"
 
 export default props => (
     <div className="container">
-        <Todo></Todo>
-        <About></About>
+        <Menu></Menu>
     </div>
 )
