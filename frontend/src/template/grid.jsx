6c311867be052117b2ac445aefd3a737cfb31a34
rafael.quelho <rafael.quelho@norus.com.br>
2020-10-18 11:03:38 -0300

    templates

---
+++ frontend/src/template/grid.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+let columnMapBootstrap = ["xs", "sm", "md", "lg"];
+
+export default class Grid extends Component {
+    toCssClasses(numbers) {
+        const cols = numbers ? numbers.split(" ") : [];
+        let classes = "";
+
+        for (let index = 0; index < 4; index++) {
+            if (cols[index]) {
+                classes += `col-${columnMapBootstrap[index]}-${cols[index]} `;
+            }
+        }
+        console.log(classes);
+        return classes.trim("");
+    }
+
+    render() {
+        const gridClasses = this.toCssClasses(this.props.cols || 12);
+        return (
+            <div className={gridClasses}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default props => (
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#"><i className="fa fa-calendar-check-o"></i>TodoApp</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
