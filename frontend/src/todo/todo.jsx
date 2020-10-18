fba02f6ff9bc6dd582bd45f4d88ab95b9dd239f7
rafael.quelho <rafael.quelho@norus.com.br>
2020-10-18 10:24:21 -0300

    todoForm and todoList

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react"
 import PageHeader from "../template/pageHeader"
+import TodoForm from "./todoForm"
+import TodoList from "./todoList"
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name="Todos" small="Create"></PageHeader>
+                <TodoForm></TodoForm>
+                <TodoList></TodoList>
             </div>
         )
     }
