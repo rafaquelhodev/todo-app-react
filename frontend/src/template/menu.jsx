399d57be86c24fffcc034b9ccc48a0c1bc22b3de
rafael.quelho <rafael.quelho@norus.com.br>
2021-02-21 09:02:20 -0300

    improve home page

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -2,16 +2,21 @@ import React from "react"
 
 export default props => (
 
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#"><i className="fa fa-calendar-check-o"></i>TodoApp</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-                <a className="nav-item nav-link" href="#/todos">Todos</a>
-                <a className="nav-item nav-link" href="#/about">About</a>
-            </div>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <a className="nav-link" href="#/todos">Todos</a>
+                            </li>
+                            <li className="nav-item">
+                    <a className="nav-link" href="#/about">About</a>
+                </li>
+            </ul>
         </div>
     </nav>
 )
