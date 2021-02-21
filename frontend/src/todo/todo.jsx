8af4cca5908dd9d46c6c3ede113790cf626bc6d5
rafael.quelho <rafael.quelho@norus.com.br>
2021-02-21 11:24:10 -0300

    add clear button

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -58,6 +59,10 @@ export default class Todo extends Component {
         this.refresh(this.state.description)
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render() {
         return (
             <div>
@@ -65,7 +70,8 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd}
-                    handleSearch={this.handleSearch}>
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}>
                     </TodoForm>
                 <TodoList
                     list={this.state.list}
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -18,6 +18,7 @@ export default props => (
                 <IconButton style="primary" icon="plus"
                     onClick={props.handleAdd}></IconButton>
                 <IconButton style="info" icon="search" onClick={props.handleSearch}></IconButton>
+                <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
             </Grid>
         </div>
     </div>
